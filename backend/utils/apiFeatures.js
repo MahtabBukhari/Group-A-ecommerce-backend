4c52303df23756c2147c0d138d358a94ac9760ba
MahtabBukhari <git config --global user.email you@example.com>
2022-09-07 10:52:29 +0500

    solution for filter on the base of price range

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -20,13 +20,21 @@ class ApiFeatures{
     }
 
     filter(){// filter category by ignore others and only category left
-        const queryStrCopy = {...this.queryStr}
-console.log(queryStrCopy)
+        const queryCopy = {...this.queryStr}
+
        const removeFields = ["keyword","page","limit"]
 
-        removeFields.forEach(key => delete queryStrCopy[key]);
-console.log(queryStrCopy)
-        this.query=this.query.find(queryStrCopy)
+        removeFields.forEach(key => delete queryCopy[key]);
+
+        // filter on the base of price we (gt,gte,lt,lte) greater than,greater than equal to, less than ,less than equal to
+        // above filter comes from font end price[gt] but it need $ sign with it so we put it as
+// first convert object to string
+        let queryStr = JSON.stringify(queryCopy)
+        console.log(queryStr)
+       queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, key=> `$${key}`)
+                          // above is reqular expression
+        console.log(queryStr)
+        this.query=this.query.find(JSON.parse(queryStr))
        
         return this
     }
