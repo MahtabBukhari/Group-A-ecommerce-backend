246ae9300941132d86d5c0e61a2e76c13796322e
MahtabBukhari <git config --global user.email you@example.com>
2022-09-07 11:12:41 +0500

    small changes

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,37 +2,40 @@
 
 const product = require("../models/productModel");
 
-
 const ErrorHandler = require("../utils/errorHandler");
-const catchAsyncError = require('../middleware/catchAsyncError');
+const catchAsyncError = require("../middleware/catchAsyncError");
 const ApiFeatures = require("../utils/apiFeatures");
 
-
 // CREATE PRODUCT --- ADMIN
-exports.createProduct =  catchAsyncError( async (req, res, next) => {
+exports.createProduct = catchAsyncError(async (req, res, next) => {
   const createdProduct = await product.create(req.body);
   res.status(201).json({
     success: true,
     createdProduct,
   });
-})
+});
 
 //GET ALL PRODUCTS
-exports.getAllProducts = catchAsyncError( async (req, res) => {
-  const contentPerPage=5
-  const apiFeatures = new ApiFeatures(product.find(),req.query).search().filter().pagination(contentPerPage)
+exports.getAllProducts = catchAsyncError(async (req, res) => {
+  const contentPerPage = 5;
+
+  const productCount = await product.countDocuments();
+
+  const apiFeatures = new ApiFeatures(product.find(), req.query)
+    .search()
+    .filter()
+    .pagination(contentPerPage);
   // const getProducts = await product.find();
   const getProducts = await apiFeatures.query;
   res.status(200).json({ success: true, getProducts });
-}
-)
+});
 //update the product  -- admin
 
-exports.updateProduct = catchAsyncError( async (req, res, next) => {
+exports.updateProduct = catchAsyncError(async (req, res, next) => {
   let updateproduct = await product.findById(req.params.id);
 
   if (!updateproduct) {
-    return  next(new ErrorHandler("Product Not Found", 404))
+    return next(new ErrorHandler("Product Not Found", 404));
     // return res.status(500).json({
     //   success: false,
     //   message: "product not found",
@@ -48,44 +51,41 @@ exports.updateProduct = catchAsyncError( async (req, res, next) => {
     success: true,
     updateproduct,
   });
-})
+});
 
-exports.deleteProduct =catchAsyncError( async (req, res,next) => {
+exports.deleteProduct = catchAsyncError(async (req, res, next) => {
   const deletedProduct = await product.findById(req.params.id);
-  
-      
-        if (!deletedProduct) {
-          return  next(new ErrorHandler("Product Not Found", 404))
-          // return res.status(500).json({
-          //   success: false,
-          //   message: "product not found",
-          // });
-        }
-        
-        await deletedProduct.remove();
-        res.status(200).json({
-          success: true,
-          message: "product is deleted successfully",
-        });
-    
 
-})
+  if (!deletedProduct) {
+    return next(new ErrorHandler("Product Not Found", 404));
+    // return res.status(500).json({
+    //   success: false,
+    //   message: "product not found",
+    // });
+  }
 
-exports.getProductDetails=catchAsyncError(async(req,res,next)=>{
+  await deletedProduct.remove();
+  res.status(200).json({
+    success: true,
+    message: "product is deleted successfully",
+  });
+});
+
+exports.getProductDetails = catchAsyncError(async (req, res, next) => {
   const getOneProduct = await product.findById(req.params.id);
-  
-  if(!getOneProduct){
+
+  if (!getOneProduct) {
     //next is a callback function that is used to call the middleware
-    return  next(new ErrorHandler("Product Not Found", 404))
+    return next(new ErrorHandler("Product Not Found", 404));
     // return res.status(500).json({
     //   success:false,
     //   message:"Product Not Found"
     // })
   }
 
-
   res.status(200).json({
-    success:true,
-    getOneProduct
-  })
-})
+    success: true,
+    getOneProduct,
+    productCount
+  });
+});
--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -1,53 +1,53 @@
-class ApiFeatures{
-    constructor(query,queryStr){
-        this.query=query,
-        this.queryStr=queryStr      // query string is portion of url after ? like  ?keyword=samosa
-    }
+class ApiFeatures {
+  constructor(query, queryStr) {
+    (this.query = query), (this.queryStr = queryStr); // query string is portion of url after ? like  ?keyword=samosa
+  }
 
-    search(){
-        const keyword = this.queryStr.keyword?
-    {name:{
-        $regex:this.queryStr.keyword, // it gives keyword value
-        $options:'i'                  // to make case insensitive
-    }}:{
-
-    }
+  search() {
+    const keyword = this.queryStr.keyword
+      ? {
+          name: {
+            $regex: this.queryStr.keyword, // it gives keyword value
+            $options: "i", // to make case insensitive
+          },
+        }
+      : {};
 
     // keyword = name:samosa
     // console.log(keyword)
-    this.query=this.query.find({...keyword})
-    return this
-    }
+    this.query = this.query.find({ ...keyword });
+    return this;
+  }
 
-    filter(){// filter category by ignore others and only category left
-        const queryCopy = {...this.queryStr}// destructure for with out refrence get quryStr 
+  filter() {
+    // filter category by ignore others and only category left
+    const queryCopy = { ...this.queryStr }; // destructure for with out refrence get quryStr
 
-       const removeFields = ["keyword","page","limit"]
+    const removeFields = ["keyword", "page", "limit"];
 
-        removeFields.forEach(key => delete queryCopy[key]);
+    removeFields.forEach((key) => delete queryCopy[key]);
 
-        // filter on the base of price we (gt,gte,lt,lte) greater than,greater than equal to, less than ,less than equal to
-        // above filter comes from font end price[gt] but it need $ sign with it so we put it as
-// first convert object to string
-        let queryStr = JSON.stringify(queryCopy)
-        // console.log(queryStr)
-       queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, key=> `$${key}`)
-                          // above is reqular expression
-        // console.log(queryStr)
-        this.query=this.query.find(JSON.parse(queryStr))
-       
-        return this
-    }
+    // filter on the base of price we (gt,gte,lt,lte) greater than,greater than equal to, less than ,less than equal to
+    // above filter comes from font end price[gt] but it need $ sign with it so we put it as
+    // first convert object to string
+    let queryStr = JSON.stringify(queryCopy);
+    // console.log(queryStr)
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (key) => `$${key}`);
+    // above is reqular expression
+    // console.log(queryStr)
+    this.query = this.query.find(JSON.parse(queryStr));
 
-    pagination(contentPerPage){
-        let currentPage = this.queryStr.page || 1
+    return this;
+  }
 
-        let skip = contentPerPage * (currentPage - 1)
+  pagination(contentPerPage) {
+    let currentPage = Number(this.queryStr.page) || 1;
 
-        this.query = this.query.limit(contentPerPage).skip(skip)
-        return this
+    let skip = contentPerPage * (currentPage - 1);
 
-    }
+    this.query = this.query.limit(contentPerPage).skip(skip);
+    return this;
+  }
 }
 
-module.exports = ApiFeatures
+module.exports = ApiFeatures;
