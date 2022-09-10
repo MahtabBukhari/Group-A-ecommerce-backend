43db08b1f3e35ec029c82ea17a8c101bd9b0f599
MahtabBukhari <git config --global user.email you@example.com>
2022-09-10 10:30:50 +0500

    logout user set cookie to null mean set cookie expires

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -48,3 +48,14 @@ exports.loginUser= catchAsyncError(async (req,res,next)=>{
     
 
 })
+
+
+exports.LogoutUser= catchAsyncError( async (req,res, next)=>{
+
+    res.cookie('token',null,{ expires:new Date(Date.now()),httpOnly:true})
+
+    res.status(200).json({
+        success:true,
+        message:"Logged Out"
+    })
+})
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { registerUser, loginUser } = require('../controllers/userController')
+const { registerUser, loginUser, LogoutUser } = require('../controllers/userController')
 const router = express.Router()
 
 
@@ -7,6 +7,7 @@ const router = express.Router()
 
 router.route('/register').post(registerUser)
 router.route('/login').post(loginUser)
+router.route('/logout').get(LogoutUser)
 
 
 module.exports = router
