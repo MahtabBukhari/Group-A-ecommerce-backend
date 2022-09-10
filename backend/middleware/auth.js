fbdb046a088a982db7dd38dc4d3e813be0e6d1d6
MahtabBukhari <git config --global user.email you@example.com>
2022-09-10 11:19:29 +0500

    athenticateRole only admin allow to get secure resources

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,22 +1,31 @@
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncError = require("./catchAsyncError");
-const jwt = require('jsonwebtoken')
-const User = require('../models/userModel')
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
+
+exports.isAuthenticatedUser = catchAsyncError(async (req, res, next) => {
+  const { token } = req.cookies;
+
+  if (!token) {
+    return next(new ErrorHandler("please first login to get resource", 401));
+  }
+
+  const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+  req.user = await User.findById(decodedToken.id);
+  next();
+});
 
 
-exports.isAuthenticatedUser = catchAsyncError( async (req, res, next) => {
 
-    const {token} = req.cookies;
+exports.authorizeRoles= (...roles)=>{
+    return (req, res, next)=>{
+        if(!roles.includes(req.user.role)){
 
-    if(!token){
-        return next(new ErrorHandler('please first login to get resource',401))
+          return  next(new ErrorHandler(`Role ${req.user.role} is not allowed to access that resource`),403)
+
+        }
+        next()
     }
-
-    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-
-    req.user= await User.findById(decodedToken.id)
-next()
-
-
     
-})
+}
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const {getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails} = require('../controllers/productController');
-const { isAuthenticatedUser } = require('../middleware/auth');
+const { isAuthenticatedUser, authorizeRoles } = require('../middleware/auth');
 
 const router = express.Router()
 
-router.route('/products').get(isAuthenticatedUser, getAllProducts)
-router.route('/product/create').post(createProduct)
-router.route('/product/:id').put(updateProduct)
-router.route('/product/:id').delete(deleteProduct)
+router.route('/products').get( getAllProducts)
+// isAuthenticatedUser function gives the (user) useing that user in authorizeRole function we check is this is admin or user 
+//Only Admin allow to create , update and delete the product 
+router.route('/product/create').post(isAuthenticatedUser, authorizeRoles("admin"),createProduct)
+router.route('/product/:id').put(isAuthenticatedUser, authorizeRoles("admin"),updateProduct)
+router.route('/product/:id').delete(isAuthenticatedUser, authorizeRoles("admin"),deleteProduct)
 router.route('/product/:id').get(getProductDetails)
 
 
