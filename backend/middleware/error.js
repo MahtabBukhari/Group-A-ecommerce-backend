c4e04c53162f36959103cee524295f41add6515c
MahtabBukhari <git config --global user.email you@example.com>
2022-09-12 10:15:01 +0500

    error handle dublicate key, wrong jwt token, exipire jwt token

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -15,6 +15,27 @@ module.exports = (err,req,res,next)=>{
         err = new ErrorHandler(message,400)
     }
 
+    //dublicate key error mean if dublicate email
+    if(err.code === 11000){
+        const message=`Dublicate ${Object.keys(err.keyValue)} Entered`
+        err = new ErrorHandler(message,400)
+    }
+
+    //Enter wrong jwt token
+    if(err.name === 'jsonwebtoken'){
+        const message=`Json Web Token Invalid, Try Again`
+        err = new ErrorHandler(message,400)
+
+    }
+
+
+    // expire json web token
+    if(err.name === 'TokenExpiredError'){
+
+        const message=`Json Web Token Expired, Try Again`
+        err = new ErrorHandler(message,400)
+    }
+
     res.status(err.statusCode).json({
         success:false,
         message:err.message
