7c05935e8de52801d034eb6bb922d474c3b93067
MahtabBukhari <git config --global user.email you@example.com>
2022-09-13 13:08:53 +0500

    create new Order in orderController and also create new route in orderRoute and add that route in app.js

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,30 @@
+const Order = require("../models/orderModel");
+const product = require("../models/productModel");
+const ErrorHandler = require("../utils/errorHandler");
+const catchAsyncError = require("../middleware/catchAsyncError");
+
+exports.newOrder = catchAsyncError(async (req, res, next) => {
+  const {
+    shippingInfo,
+    orderItems,
+    paymentInfo,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+
+  const order = await Order.create({
+    shippingInfo,
+    orderItems,
+    paymentInfo,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+    user: req.user._id,
+    paidAt: Date.now(),
+  });
+
+  res.status(200).json({ success: true, order });
+});
