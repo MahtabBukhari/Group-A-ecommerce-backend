929963444de7cef8a6f762e3e0a2c997723f3b5e
MahtabBukhari <git config --global user.email you@example.com>
2022-09-14 09:51:03 +0500

    get Single order to see detail and user get own orders details

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -2,6 +2,7 @@ const Order = require("../models/orderModel");
 const product = require("../models/productModel");
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncError = require("../middleware/catchAsyncError");
+const { rawListeners } = require("../models/userModel");
 
 exports.newOrder = catchAsyncError(async (req, res, next) => {
   const {
@@ -28,3 +29,35 @@ exports.newOrder = catchAsyncError(async (req, res, next) => {
 
   res.status(200).json({ success: true, order });
 });
+
+
+//Get single order 
+
+exports.getSingleOrder = catchAsyncError( async(req,res,next)=>{
+
+  //in Order model only user id present if we want to get the user data from that id then we use (populate) function
+  const order = await Order.findById(req.params.id).populate("user","name email")
+                                                           // user that is in Schema
+   if(!order){
+    return next(new ErrorHandler("Order Not Found",404))
+   }
+
+   res.status(200).json({
+    success:true,
+    order
+  })
+})
+
+
+
+//user own order
+
+exports.myOrders = catchAsyncError( async( req, res, next)=>{
+
+  const orders = await Order.find({user:req.user.id})
+
+  res.status(200).json({
+    success:true,
+    orders
+  })
+})
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,11 +1,16 @@
 const express = require('express');
-const { newOrder } = require('../controllers/orderController');
+const { newOrder, getSingleOrder, myOrders } = require('../controllers/orderController');
 const router = express.Router();
 
 const {isAuthenticatedUser,authorizeRoles} = require('../middleware/auth')
 
 
 router.route('/order/new').post(isAuthenticatedUser, newOrder)
+
+router.route('/order/myOrder').get(isAuthenticatedUser, myOrders)
+
+router.route('/order/:id').get(isAuthenticatedUser,getSingleOrder)
+
 
 
 
